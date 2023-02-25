c238e4d244321e57fc44164069d7821c06efcf78
Bilkish-akther <bilkishakther921@gmail.com>
2023-02-25 13:11:20 +0600

    first commit

---
+++ scope.js
@@ -0,0 +1,33 @@
+// global scope
+
+// block scope
+// local scope
+
+
+
+
+{
+    // var roll = 12
+    // console.log(roll)
+}
+// console.log(roll)
+
+// function add(){
+//      const a = 5;
+// }
+
+// console.log(a)
+
+let Name = 'mehedy'
+const isMale = true;
+
+{
+     const isMale = false;
+     console.log(isMale)
+}
+
+
+
+
+
+
